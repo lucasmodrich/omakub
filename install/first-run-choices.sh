01f5227fb27a17bc19c44b026f4fb5b673d4f9bb
Lucas Modrich <lucas@modrich.com>
2025-03-05 14:18:30 +1100

    Update version to include -lm to indicate customisation. Also remove default database selection

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -10,5 +10,5 @@ SELECTED_LANGUAGES="Ruby on Rails","Node.js"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
-SELECTED_DBS="MySQL,Redis"
+SELECTED_DBS="" #"MySQL,Redis"
 export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
